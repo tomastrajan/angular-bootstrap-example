47faf4507175fb0c8422108ee092cef79641b67b
Tomas Trajan <tomas.trajan@gmail.com>
2018-05-14 12:42:15 +0800

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'abe-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'abe';
+}
